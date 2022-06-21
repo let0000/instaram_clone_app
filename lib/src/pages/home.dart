6332e1a698d7f1fc64589c0c54ed73b1571204bc
jinho <let0000>
2022-06-21 16:51:17 +0900

    홈화면 UI 구현 cached_network_image 라이브러리 활용 - 아바타 위젯 구현 expandable_text 라이브러리 활용 - 인스타 설명 텍스트 구현

---
+++ lib/src/pages/home.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:instaram_clone/src/components/avatar_widget.dart';
+import 'package:instaram_clone/src/components/image_data.dart';
+import 'package:instaram_clone/src/components/post_widget.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  Widget _myStory() {
+    return Stack(
+      children: [
+        AvatarWidget(
+            type: AvatarType.TYPE2,
+            size: 70,
+            thumbPath:
+                'https://image.fmkorea.com/files/attach/new/20201027/3655109/1517176875/3166657926/247c418a79b196df884de1e5f2d8c4e4.jpeg'),
+        Positioned(
+            right: 5,
+            bottom: 0,
+            child: Container(
+              width: 25,
+              height: 25,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                  border: Border.all(color: Colors.white, width: 2)),
+              child: const Center(
+                child: Text(
+                  '+',
+                  style:
+                      TextStyle(fontSize: 20, color: Colors.white, height: 1.1),
+                ),
+              ),
+            ))
+      ],
+    );
+  }
+  
+  Widget _storyBoardList() {
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(children: [
+          const SizedBox(width: 20),
+          _myStory(),
+          const SizedBox(width: 5),
+          ...List.generate(
+              10,
+              (index) => AvatarWidget(
+                  type: AvatarType.TYPE1,
+                  thumbPath:
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7nEqRASiiH3T-PaJhG3uy_rwJjpvs1as1vA&usqp=CAU'))
+        ]));
+  }
+  
+  Widget _postList() {
+    return Column(
+      children: List.generate(10, (index) => PostWidget()).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: ImageData(IconPath.logo, width: 270),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: ImageData(IconPath.directMessage, width: 50),
+            ),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          _storyBoardList(),
+          _postList()
+        ],
+      ),
+    );
+  }
+}
