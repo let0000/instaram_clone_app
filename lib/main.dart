2afb160bf7a6ef7447d4f02d41143d7eaeb405e6
jinho <let0000>
2022-07-08 18:08:02 +0900

    파이어베이스 세팅 및 구글 로그인 기능 구현 Friebase Cli 를 이용하여 파이어베이스 연동 firebase_core firebase_auth google_sign_in 라이브러리 활용

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:instaram_clone/firebase_options.dart';
 import 'package:instaram_clone/src/binding/init_bindings.dart';
-import 'package:instaram_clone/src/controller/bottom_nav_controller.dart';
+import 'package:instaram_clone/src/root.dart';
 
-import 'src/app.dart';
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -27,7 +29,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       initialBinding: InitBinding(),
-      home: const App(),
+      home: const Root(),
     );
   }
 }
